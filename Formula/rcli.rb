306c9a47fae3537d908f67e14e82084d9713483d
pat-s <patrick.schratz@gmail.com>
2021-12-07 23:02:15 +0100

    add brew formula

--- Formula/rcli.rb
+++ Formula/rcli.rb
@@ -1,14 +1,13 @@
 class Rcli < Formula
   desc "Command Line tool to install and switch between R versions"
   homepage "https://github.com/pat-s/rcli"
-  url "file:///Users/pjs/git/rcli/bin/rcli-v0.1.0.tar.gz"
-  sha256 "e7efa5859e9c91a65dfdcf2d8122f95c48acf1d3679439cff0a26226c94fb265"
+  url "https://github.com/pat-s/rcli/archive/refs/tags/v0.1.0-alpha.tar.gz"
+  sha256 "83bd543026d7a319ac22f12d58027e21740afa4d8d71333528a7dc094a0c6968"
   head "https://github.com/pat-s/rcli.git"
   license "MIT License"
 
-  depends_on "shc" => :build
-
   depends_on "bash"
+  depends_on "curl"
 
   def install
     bin.install "rcli"
