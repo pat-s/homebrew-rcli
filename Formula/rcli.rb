615ee0f12f9159cb6e9c1f14bb82db46ff8c96e2
pat-s <patrick.schratz@gmail.com>
2021-12-09 10:54:12 +0100

    update to v0.1.0-beta

--- Formula/rcli.rb
+++ Formula/rcli.rb
@@ -1,12 +1,11 @@
 class Rcli < Formula
   desc "Command Line tool to install and switch between R versions"
   homepage "https://github.com/pat-s/rcli"
-  url "https://github.com/pat-s/rcli/releases/download/v0.1.0-alpha/rcli"
-  sha256 "d39b7d080dadb737cd05bbbb310d0c247ba11584a47d957e5d119b646eaeb9f0"
+  url "https://github.com/pat-s/rcli/releases/download/v0.1.0-beta/rcli"
+  sha256 "4c510562c6f1a0efef331313416c443a99d705f6ac3fdd3a4f67491eeb40959b"
   head "https://github.com/pat-s/rcli.git"
   license "MIT License"
 
-  depends_on "bash"
   depends_on "curl"
 
   def install
