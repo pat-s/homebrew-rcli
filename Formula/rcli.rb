543dbae03bcb8c7fba7e669a4872189cbe716131
pat-s <patrick.schratz@gmail.com>
2021-12-10 00:11:17 +0100

    MIT license

--- Formula/rcli.rb
+++ Formula/rcli.rb
@@ -3,7 +3,7 @@ class Rcli < Formula
   homepage "https://github.com/pat-s/rcli"
   url "https://github.com/pat-s/rcli/releases/download/v0.2.0-1/rcli"
   sha256 "41fafb149a0a93a6ce3db4c32ab6373275f059ac5148206f9c87b8cc719df752"
-  license "LGPL-3.0"
+  license "MIT"
   version "v0.2.0-1"
 
   def install
