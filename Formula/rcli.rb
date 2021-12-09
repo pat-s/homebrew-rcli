7ec1142b51b89bade9e5780aadf874b441dcebaf
Patrick Schratz <patrick.schratz@gmail.com>
2021-12-09 23:36:00 +0100

    update rcli formula

--- Formula/rcli.rb
+++ Formula/rcli.rb
@@ -2,7 +2,7 @@ class Rcli < Formula
   desc "Command Line tool to install and switch between R versions"
   homepage "https://github.com/pat-s/rcli"
   url "https://github.com/pat-s/rcli/releases/download/v0.2.0-1/rcli"
-  sha256 "xxxxxxxxxxxxx"
+  sha256 "41fafb149a0a93a6ce3db4c32ab6373275f059ac5148206f9c87b8cc719df752"
   license "LGPL-3.0"
   version "v0.2.0-1"
 
